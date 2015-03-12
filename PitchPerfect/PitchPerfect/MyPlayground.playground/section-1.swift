3a2aeeb7f6ccecfcfab5afa93a535b7d163a9aa9
Moath othman <m_o_3_1989@yahoo.com>
2015-03-12 23:05:22 +0300

    Committed by Udacity

---
+++ PitchPerfect/PitchPerfect/MyPlayground.playground/section-1.swift
@@ -0,0 +1,14 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var o:Int!
+var x :Int
+
+o = "124".toInt()
+if var y  = o {
+println(y)
+}
+
